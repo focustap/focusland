af7d68240efecbddbe62ef86b2aeef398c911a38
focustap <xfocustapx@gmail.com>
2026-03-15 12:19:31 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [react()],
   base: "/focusland/",
+  build: {
+    outDir: "dist"
+  },
   server: {
     port: 5173
   }
