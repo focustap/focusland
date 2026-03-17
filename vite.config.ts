ffb47d70463bb3a27d37ba81c27179efc604ed46
focustap <xfocustapx@gmail.com>
2026-03-17 11:19:38 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,14 @@ export default defineConfig({
   plugins: [react()],
   base: "/focusland/",
   build: {
-    outDir: "dist"
+    outDir: "dist",
+    rollupOptions: {
+      output: {
+        entryFileNames: "assets/[name].js",
+        chunkFileNames: "assets/[name].js",
+        assetFileNames: "assets/[name][extname]"
+      }
+    }
   },
   server: {
     port: 5173
